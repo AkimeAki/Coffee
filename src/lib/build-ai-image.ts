5b8fb1ccd821afadd0de1db961d5f104ff613ba4
Aki <62400162+AkimeAki@users.noreply.github.com>
2023-11-27 02:59:32 +0900

    画像生成処理作成

--- src/lib/build-ai-image.ts
+++ src/lib/build-ai-image.ts
@@ -1,8 +1,15 @@
 import type { AstroIntegration } from "astro";
 import { loadEnv } from "vite";
-import { S3Client, ListBucketsCommand, ListObjectsV2Command } from "@aws-sdk/client-s3";
+import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
+import { getListContents, getContentsDetail } from "./microcms.ts";
+import OpenAI from "openai";
+import cheerio from "cheerio";
+import type { Blog } from "./microcms.ts";
 
 const env = loadEnv("", process.cwd());
+const openai = new OpenAI({
+	apiKey: env.VITE_OPENAPI_KEY
+});
 
 const S3 = new S3Client({
 	region: "auto",
@@ -21,10 +28,58 @@ export default function (): AstroIntegration {
 				logger.info("Ai画像未生成の記事を検索中");
 				try {
 					const r2Data = await S3.send(new ListObjectsV2Command({ Bucket: "coffee" }));
+					if (r2Data.Contents === undefined) {
+						throw new Error();
+					}
+
+					const posts = await getListContents<Blog>("blogs");
+					const ids: string[] = [];
+					posts.contents.forEach((post) => {
+						ids.push(post.id);
+					});
+
+					const ungeneratedIds = [];
+
+					ids.forEach((id) => {
+						let generated = false;
+
+						r2Data.Contents?.forEach((content) => {
+							if (`${id}.png` === content.Key) {
+								generated = true;
+							}
+						});
+
+						if (!generated) {
+							ungeneratedIds.push(id);
+						}
+					});
+
+					logger.info("以下のIDについて画像生成を行います。");
+					logger.info(ungeneratedIds.join(", "));
+
+					logger.info("Ai画像生成中");
+
+					for (const id of ungeneratedIds) {
+						const post = await getContentsDetail<Blog>("blogs", id);
+						const $ = cheerio.load(post.contents);
+
+						const blogContent = $.text();
+
+						const prompt = `次の文章からいくつかキーワードを抽出し、そのキーワードに沿って画像を生成してください。現実的な雰囲気でお願いします。\n\n${blogContent}`;
+
+						const imageResponse = await openai.images.generate({
+							model: "dall-e-3",
+							prompt,
+							n: 1,
+							size: "1792x1024"
+						});
+
+						console.log(imageResponse);
+					}
 				} catch (e) {
-					logger.info("R2アクセス時にエラーが発生しました。");
+					logger.error(e);
+					logger.info("エラーが発生しました。");
 				}
-				logger.info("Ai画像生成中");
 			}
 		}
 	};
--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -1,10 +1,13 @@
 import { createClient } from "microcms-js-sdk";
-import { nullToUndefined } from "@/lib/nullToUndefined";
+import { nullToUndefined } from "./nullToUndefined";
+import { loadEnv } from "vite";
 import type { MicroCMSContentId, MicroCMSDate, MicroCMSListResponse, MicroCMSQueries } from "microcms-js-sdk";
 
+const env = loadEnv("", process.cwd());
+
 const client = createClient({
-	serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
-	apiKey: import.meta.env.MICROCMS_API_KEY
+	serviceDomain: import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN ?? env.VITE_MICROCMS_SERVICE_DOMAIN,
+	apiKey: import.meta.env.VITE_MICROCMS_API_KEY ?? env.VITE_MICROCMS_API_KEY
 });
 
 export interface Blog {
