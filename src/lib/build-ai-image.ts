bed6cc75236917b40e2af5b3c0011e47384df4ec
Aki <62400162+AkimeAki@users.noreply.github.com>
2023-11-27 04:30:35 +0900

    デザイン修正

--- src/lib/build-ai-image.ts
+++ src/lib/build-ai-image.ts
@@ -1,6 +1,6 @@
 import type { AstroIntegration } from "astro";
 import { loadEnv } from "vite";
-import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
+import { S3Client, ListObjectsV2Command, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getListContents, getContentsDetail } from "./microcms.ts";
 import OpenAI from "openai";
 import cheerio from "cheerio";
@@ -8,7 +8,7 @@ import type { Blog } from "./microcms.ts";
 
 const env = loadEnv("", process.cwd());
 const openai = new OpenAI({
-	apiKey: env.VITE_OPENAPI_KEY
+	apiKey: env.VITE_OPENAPI_KEY ?? ""
 });
 
 const S3 = new S3Client({
@@ -38,7 +38,7 @@ export default function (): AstroIntegration {
 						ids.push(post.id);
 					});
 
-					const ungeneratedIds = [];
+					const ungeneratedIds: string[] = [];
 
 					ids.forEach((id) => {
 						let generated = false;
@@ -54,30 +54,53 @@ export default function (): AstroIntegration {
 						}
 					});
 
-					logger.info("以下のIDについて画像生成を行います。");
-					logger.info(ungeneratedIds.join(", "));
+					if (ungeneratedIds.length !== 0) {
+						logger.info("以下のIDについて画像生成を行います。");
+						logger.info(ungeneratedIds.join(", "));
 
-					logger.info("Ai画像生成中");
+						logger.info("Ai画像生成中");
 
-					for (const id of ungeneratedIds) {
-						const post = await getContentsDetail<Blog>("blogs", id);
-						const $ = cheerio.load(post.contents);
+						for (const id of ungeneratedIds) {
+							const post = await getContentsDetail<Blog>("blogs", id);
+							const $ = cheerio.load(post.contents);
 
-						const blogContent = $.text();
+							const blogContent = $.text();
 
-						const prompt = `次の文章からいくつかキーワードを抽出し、そのキーワードに沿って画像を生成してください。現実的な雰囲気でお願いします。\n\n${blogContent}`;
+							const prompt = `Extract some keywords from the following text and generate images without including text according to the keywords. The atmosphere of the image should be in the style of a real-life photograph.\n\n"${blogContent}"`;
 
-						const imageResponse = await openai.images.generate({
-							model: "dall-e-3",
-							prompt,
-							n: 1,
-							size: "1792x1024"
-						});
+							const imageResponse = await openai.images.generate({
+								model: "dall-e-3",
+								prompt,
+								n: 1,
+								size: "1792x1024"
+							});
 
-						console.log(imageResponse);
+							if (imageResponse.data[0] === undefined) {
+								throw new Error();
+							}
+
+							const imageUrl = imageResponse.data[0].url;
+
+							if (imageUrl === undefined) {
+								throw new Error();
+							}
+
+							const res = await fetch(imageUrl);
+							const arrayBuffer = await res.arrayBuffer();
+							const buffer = Buffer.from(arrayBuffer);
+
+							await S3.send(
+								new PutObjectCommand({
+									Body: buffer,
+									Bucket: "coffee",
+									Key: `${id}.png`,
+									ContentType: "image/png"
+								})
+							);
+						}
 					}
 				} catch (e) {
-					logger.error(e);
+					logger.error(String(e));
 					logger.info("エラーが発生しました。");
 				}
 			}
